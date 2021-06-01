fedde286f968508d2b6257d451a2473869030c73
Zach <zHixson.xbdi@gmail.com>
2021-05-31 22:57:18 -0500

    Added back in debug sprites, as well as code in "package.js" to remove the debug sprites on build

--- package.js
+++ package.js
@@ -9,8 +9,21 @@ if (!fs.existsSync(BUILD_DIR)){
     fs.mkdirSync(BUILD_DIR);
 }
 
+//copy over music folder
+let MUSIC_DEST = BUILD_DIR + '/music';
+if (!fs.existsSync(MUSIC_DEST)){
+    fs.mkdirSync(MUSIC_DEST);
+}
+
+fs.readdirSync('./music').forEach(fileName =>{
+    fs.copyFileSync('./music/' + fileName, MUSIC_DEST + '/' + fileName);
+})
+
 let bitsyData = fs.readFileSync('./jamies_abyss.bitsy', {encoding: 'utf8'});
 let htmlData = fs.readFileSync('./jamies_abyss.html', {encoding: 'utf8'});
+
+//remove debug sprites
+bitsyData = bitsyData.replace(/10000000\r\n(00000000\r\n)+00000001/mg, new Array(8).fill(new Array(8).fill('0').join('')).join('\n'))
 
 //merge bitsy + html
 let merged = htmlData.replace(/IMPORT jamies_abyss.bitsy/, bitsyData);
@@ -32,14 +45,8 @@ for (let i = 0; i < scriptRefs.length; i++){
     }
 }
 
+//remove console.logs() and single line comments
+merged = merged.replace(/console\.log\(.+\);*/g, '');
+merged = merged.replace(/\/\/.+/g, '');
+
 fs.writeFileSync(BUILD_DIR + "/index.html", merged);
-
-//copy over music folder
-let MUSIC_DEST = BUILD_DIR + '/music';
-if (!fs.existsSync(MUSIC_DEST)){
-    fs.mkdirSync(MUSIC_DEST);
-}
-
-fs.readdirSync('./music').forEach(fileName =>{
-    fs.copyFileSync('./music/' + fileName, MUSIC_DEST + '/' + fileName);
-})
